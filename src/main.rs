b8b3891536073ba4ddb974639a6890145b980c24
aurumic <63585852+aurumic@users.noreply.github.com>
2025-11-05 22:35:01 +0200

    basic runtime argument support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use netdec::is_valid_ip_range;
+
 fn main() {
-    println!("Hello, world!");
+  let Some(arg) = env::args().nth(1) else {
+    eprintln!("usage: cargo run -- <INPUT>");
+    std::process::exit(2);
+  };
+
+  if is_valid_ip_range(&arg) {
+    println!("valid");
+    std::process::exit(0);
+  } else {
+    println!("invalid");
+    std::process::exit(1);
+  }
 }
